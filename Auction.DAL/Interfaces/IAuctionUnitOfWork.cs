54b2a6bf1ed7f02c08c9c4cc8a1ffdb30d391be7
closirr <closirr@gmail.com>
2017-05-12 14:41:26 +0300

    UOW implemented

---
+++ Auction.DAL/Interfaces/IAuctionUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auction.DAL.Entities;
+
+namespace Auction.DAL.Interfaces
+{
+    public interface IAuctionUnitOfWork : IDisposable
+    {
+        IRepository<Lot> Lots { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<Bid> Bids { get; }
+        IRepository<ShippingData> ShippingDatas { get; }
+        void Save();
+    }
+}
---
+++ Auction.DAL/UnitsOfWork/AuctionUnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auction.DAL.Contexts;
+using Auction.DAL.Entities;
+using Auction.DAL.Interfaces;
+using Auction.DAL.Repositories;
+
+namespace Auction.DAL.UnitsOfWork
+{
+    class AuctionUnitOfWork:IAuctionUnitOfWork
+    {
+        private AuctionContext db;
+        private LotRepository LotRepository;
+        private CategoryRepository CategoryRepository;
+        private BidRepository BidRepository;
+        private ShippingDataRepository ShippingDataRepository;
+
+        public AuctionUnitOfWork(string connectionString)
+        {
+            db = new AuctionContext(connectionString);
+        }
+
+        public IRepository<Lot> Lots
+        {
+            get
+            {
+                if (LotRepository == null)
+                    LotRepository = new LotRepository(db);
+                return LotRepository;
+            }
+        }
+        public IRepository<Category> Categories
+        {
+            get
+            {
+                if (CategoryRepository == null)
+                    CategoryRepository = new CategoryRepository(db);
+                return CategoryRepository;
+            }
+        }
+        public IRepository<Bid> Bids
+        {
+            get
+            {
+                if (BidRepository == null)
+                    BidRepository = new BidRepository(db);
+                return BidRepository;
+            }
+        }
+
+        public IRepository<ShippingData> ShippingDatas
+        {
+            get
+            {
+                if (ShippingDataRepository == null)
+                    ShippingDataRepository = new ShippingDataRepository(db);
+                return ShippingDataRepository;
+            }
+        }
+
+
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
